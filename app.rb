46e2502c96579057d4901013a159a7806f685259
Brian Pompey <brianthescholar@gmail.com>
2019-12-03 19:00:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,4 +3,9 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  get "/reversename/:name" do
+    @reversedname = params[:name].reverse_inplace
+
+    "#{@reversedname}"
+  end
 end
