23cfa80a76d404a0e28106276576339321a7c3cb
Brian Pompey <brianthescholar@gmail.com>
2019-12-03 19:24:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,11 +16,12 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @number = params[:number].to_i
     @phrase = params[:phrase]
-    "#{@phrase}" * @number 
+    "#{@phrase}" * @number
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @makelist =
+    @phrase = params[:word1] + " " + params[:word2] + " " + params[:word3] + " " + params[:word4] + " " + params[:word5]
+   "#{@phrase}."
   end
 
   get '/:operation/:number1/:number2' do
