93baafa6911baba14f8afabf0376f1a518aa21e2
Brian Pompey <brianthescholar@gmail.com>
2019-12-03 19:24:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,21 +25,19 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    operation = params[:operation]
-    number1 = params[:number1].to_i
-    number2 = params[:number2].to_i
-    if operation == "add"
-      answer = number1 + number2
-    elsif operation == "subtract"
-      answer = number1 - number2
-    elsif operation == "multiply"
-      answer = number1 * number2
-    elsif operation == "divide"
-      answer = number1 / number2
-    else
-      answer = "Unable to perform this operation"
+    @operation = params[:operation]
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+    case @operation
+    when "add"
+      "#{@num1 + @num2}"
+    when "subtract"
+      "#{@num1 - @num2}"
+    when "multiply"
+      "#{@num1 * @num2}"
+    when "divide"
+      "#{@num1 / @num2}"
     end
-    answer.to_s
   end
 
 end
