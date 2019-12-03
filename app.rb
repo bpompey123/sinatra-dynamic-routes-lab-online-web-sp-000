c13aaa3d7e7c645aa7bcfd365068e55e2ed6d97d
Brian Pompey <brianthescholar@gmail.com>
2019-12-03 19:02:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ class App < Sinatra::Base
   # Write your code here!
 
   get "/reversename/:name" do
-    @reversedname = params[:name].reverse_inplace
+    @reversedname = params[:name].reverse
 
     "#{@reversedname}"
   end
