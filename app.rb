4efcb7d7645b6f5cf37d4e60162d6d9e8c524067
Brian Pompey <brianthescholar@gmail.com>
2019-12-03 19:15:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    @phrasenumber = params[:number] times.do
+    @phrasenumber = params[:number].to_i times.do
       puts params[:phrase]
     end
 
@@ -24,6 +24,10 @@ class App < Sinatra::Base
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @makelist = 
+    @makelist =
+  end
+
+  get '/:operation/:number1/:number2' do
+    @operator = params[:number1] params[:number2]
   end
 end
